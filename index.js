535310fc242bfccf91ec31f08407c8b41d343a6b
Raí Santos <140329135+itzraiss@users.noreply.github.com>
2024-03-10 10:51:55 -0300

    improve

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
+// src/CopilotImageCreator.js
+export default class CopilotImageCreator {
+  // class implementation here
+}
+
+// index.js
 import CopilotImageCreator from './src/CopilotImageCreator.js';
 
 export { CopilotImageCreator };
+
+// Now, you can use the default export directly
+// or import it as named export based on your preference
 export default CopilotImageCreator;
